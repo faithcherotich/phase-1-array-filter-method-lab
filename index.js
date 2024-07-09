7368e5ac3bf7b26df5b5bad826a46e098685a1eb
faithcherotich <faithcherotich910@gmail.com>
2024-07-09 12:32:07 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+//Code your solution here
+function findMatching(drivers, searchString) {
+    return drivers.filter(driver => driver.toLowerCase() === searchString.toLowerCase());
+}
+
+
+const drivers1 = ['Alice', 'Bob', 'Charlie', 'alice', 'ALICE'];
+const searchString1 = 'Alice';
+
+console.log(findMatching(drivers1, searchString1));  
+console.log(findMatching(drivers1, 'David'));
+
+//fuzzyMatch()
+function fuzzyMatch(drivers, searchString) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchString.toLowerCase()));
+}
+
+const drivers2 = ['Alice', 'Bob', 'Charlie', 'alice', 'ALICE'];
+const searchString2 = 'Al';
+
+console.log(fuzzyMatch(drivers2, searchString2));  
+console.log(fuzzyMatch(drivers2, 'li')); 
+
+//matchName
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+const drivers3 = [
+    { name: 'Alice', hometown: 'Kericho' },
+    { name: 'Bob', hometown: 'Nairobi' },
+    { name: 'Charlie', hometown: 'Nakuru' }
+];
+const nameToMatch = 'Alice';
+console.log(matchName(drivers3, nameToMatch));  
+console.log(matchName(drivers3, 'David'));
